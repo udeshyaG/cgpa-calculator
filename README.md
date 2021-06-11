801303185922912ee9b907ccda6a3bc48e6b53ea
Udeshya <udeshyagiri@gmail.com>
2021-06-11 16:36:14 +0530

    added readme

---
+++ README.md
@@ -0,0 +1,15 @@
+# My Kubernetes Project 🕸
+
+## Technology Used 🌟
+
+- React (frontend)
+- Node JS (backend)
+- Postgres (database) 🐘
+- Redis (caching) ❤
+- Kubernetes
+- Docker 🐳
+- Travis CI 👷‍♂️
+
+## Features
+
+-
--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,7 +1,7 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('Renders the heading of the app', () => {
   render(<App />);
   const headingElement = screen.getByText('CGPA Calculator');
   expect(headingElement).toBeInTheDocument();
